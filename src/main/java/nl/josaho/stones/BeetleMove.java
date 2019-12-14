396daa6a3b50a5f4c83ae842d41fbaf808d3d484
Joel Hoekstra <jlsahoekstra@gmail.com>
2019-12-14 11:48:05 +0100

    Added 1 move distance to beetle move

--- src/main/java/nl/josaho/stones/BeetleMove.java
+++ src/main/java/nl/josaho/stones/BeetleMove.java
@@ -5,7 +5,7 @@ import nl.josaho.Coord;
 
 public class BeetleMove  {
     public static boolean isMoveAllowed(Board board, Coord from, Coord to) {
-        // beetle is allowed to move everywhere
-        return Move.isShiftConnected(board, from, to);//Move.isValidShift(board, from, to);
+        // beetle is allowed to move everywhere but only 1 field away
+        return Move.isShiftConnected(board, from, to) && Move.inShiftRange(board, from, to, 1);//Move.isValidShift(board, from, to);
     }
 }
